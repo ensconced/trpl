d8c91f853d6f8d42d91e1e8ae59dde4df6d0eb78
Joe Barnett <joebarnett876@gmail.com>
2021-12-11 10:50:56 +0000

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,31 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        email: String::from("foo"),
+        ..user1
+    };
+
+    println!("username is {}", user1.username);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
