a403f5e27d8ceaff56a66b214939459d16e817a7
Joe Barnett <joebarnett876@gmail.com>
2021-12-12 11:15:50 +0000

    strings

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2;
-    println!("{}", s1);
-    println!("{}", s3);
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{}", s);
 }
